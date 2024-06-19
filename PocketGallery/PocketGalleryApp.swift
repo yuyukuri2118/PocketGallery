1c21c06fbce1c8122a9165f4bcbe5c95b9c8306e
yuyukuri2118 <kurikuri1225yu@gmail.com>
2024-06-19 16:07:33 +0900

    Initial Commit

---
+++ PocketGallery/PocketGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketGalleryApp.swift
+//  PocketGallery
+//
+//  Created by aijus on 2024/06/19.
+//
+
+import SwiftUI
+
+@main
+struct PocketGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
